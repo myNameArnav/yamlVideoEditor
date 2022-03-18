63cc8374904831abe56d549764c60829400b53c6
Arnav Jain <arnjain3@gmail.com>
2022-03-18 20:12:47 +0530

    it works

--- website/server.py
+++ website/server.py
@@ -1,7 +1,13 @@
 from flask import Flask, redirect, render_template, request, url_for
 from submit import strToYAML
+import os
 
 app = Flask(__name__)
+
+# ! global variables
+global yamlName
+global uploadsDir
+
 
 def saveFile(yaml, name):
     file = open(r"static/yaml/" + name + ".yaml", "w")
@@ -33,13 +39,30 @@ def editor():
 @app.route("/editor/<name>/assetUploading", methods = ["GET", "POST"])
 def assetLoading(name):
     if request.method == "POST":
-        pass
+        if request.files:
+            dictionary = strToYAML(readFile(yamlName))
+            dictionary = dictionary["Project"]["assets"]
+            vid = list(dictionary["video"].keys())
+            img = list(dictionary["image"].keys())
+            vidUpload = request.files["vidAsset"]
+            imgUpload = request.files["imgAsset"]
+            UploadDir = "media"
+            for vids in vid:
+                vidUpload.save(os.path.join(UploadDir + "\\" + vids))
+            for imgs in img:
+                imgUpload.save(os.path.join(UploadDir + "\\" + imgs))
+            # print(vidUpload, imgUpload)
+            return redirect(url_for("render", name = name))
     else:
         dictionary = strToYAML(readFile(yamlName))
         dictionary = dictionary["Project"]["assets"]
         vid = list(dictionary["video"].keys())
         img = list(dictionary["image"].keys())
         return render_template("assetLoading.html", vid = vid, img = img, name = name)
+
+@app.route("/editor/<name>/render")
+def render(name):
+    return render_template("render.html", name = name)
 
 if __name__ == "__main__":
     app.run(debug=True)
